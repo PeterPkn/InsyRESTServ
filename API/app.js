e9b674a897a98f473fab092d428c7470f7dd526a
peterpkn <petsch.pb@gmail.com>
2022-02-28 11:15:28 +0100

    website

--- API/app.js
+++ API/app.js
@@ -1,9 +1,14 @@
-
+var fs = require('fs');
+var bodyParser = require("body-parser");
+var parser = require('xml2js');
 const express = require('express')
 const app = express()
 const port = 5000
 
+app.use(bodyParser.json())
+
 const cors = require('cors');
+const { json } = require('express');
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
@@ -11,6 +16,32 @@ app.use(cors({
 app.get('/', (req, res) => {
   res.send({data: 'Hello World!'})
 })
+
+async function getXMLFiletoJSON(){
+  let res = {}
+  let xml = fs.readFileSync( './buchladen.xml').toString()
+  res = await parser.parseStringPromise(xml)
+  return res
+}
+
+app.get('/alldata', async (req, res) => {
+  let data = await getXMLFiletoJSON();
+  console.log(data.buchladen.buchabteilung.keys());
+  res.send({data: data});
+});
+
+app.get('/sections', async (req, res) => {
+  let data = await getXMLFiletoJSON();
+  console.log(Object.keys(data.buchladen.buchabteilung[0]));
+  res.send({data: Object.keys(data.buchladen.buchabteilung[0])});
+});
+
+app.post("/abteilung", async (req, res) => {
+  let data = await getXMLFiletoJSON();
+  //console.log(data.buchladen.buchabteilung[0][[req.body.abteilung]][0].buch);
+  res.send({data: data.buchladen.buchabteilung[0][req.body.abteilung][0].buch});
+});
+
 
 
 app.get('/infos', (req, res) => {
--- insyrestserv/src/components/main.js
+++ insyrestserv/src/components/main.js
@@ -1,36 +1,112 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import {FormControl, InputLabel, MenuItem, Select, Divider, Box, AppBar, Toolbar, Typography, Table, TableRow, TableCell, TableBody, TableHead} from "@mui/material"
 
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  return response.json();
+}
 
 export default function Main(){
 
     
     const [loaded, setLoaded] = useState(false);
-
-    const [data, setData] = useState();
+    const [selectedAbteilung, setSelectedAbteilung] = useState();
+    const [abteilungBooks, setAbteilungBooks] = useState();
+    const [abteilungen, setAbteilungen] = useState();
 
     
     async function getData(){
-        return await fetch("http://localhost:5000/").then(res=>res.json()).then(data=>data.data).then(d=>setData(d));
+        return await fetch("http://localhost:5000/").then(res=>res.json()).then(data=>data.data).then(d=>setAbteilungen(d));
       }
 
     async function getInfo(){
-        return await fetch("http://localhost:5000/infos").then(res=>res.json()).then(data=>data.data).then(d=>setData(d));
+        return await fetch("http://localhost:5000/infos").then(res=>res.json()).then(data=>data.data).then(d=>setAbteilungen(d));
+      }
+    
+      async function getData(){
+        return await fetch("http://localhost:5000/sections").then(res=>res.json()).then(data=>data.data).then(d=>setAbteilungen(d));
+      }
+
+      async function getBooks(){
+        return await fetch("http://localhost:5000/sections").then(res=>res.json()).then(data=>data.data).then(d=>setAbteilungen(d));
       }
 
     useEffect(()=>{
         setLoaded(true);
-    }, [data]);
+    }, [abteilungen]);
 
-    if(!loaded){
-    getInfo();
-}
 
+    useEffect(()=>{
+      if(!loaded){
+        getData();
+      }
+    }, [loaded]);
+
+    useEffect(()=>{
+      if (selectedAbteilung){
+        postData("http://localhost:5000/abteilung", {abteilung: selectedAbteilung}).then(data=>data.data).then(d=>setAbteilungBooks(d));
+      }
+
+    }, [selectedAbteilung]);
 
 
 return(
     <div>
-    {loaded && <p>{data?.join(', ')}</p> }
+    <AppBar>
+      <Toolbar>
+        <Typography sx={{ flexGrow: 1, fontWeight:"bold" }} >Buchladen</Typography>
+        <Typography>Peter Bezak</Typography>
+      </Toolbar>
+    </AppBar>
+    <Box sx={{padding:"5%"}}>
+      <div style={{width:'100%', display:'flex', justifyContent:'center'}}>
+      <FormControl>
+        <InputLabel id="abteilungen-select-label">Abteilungen</InputLabel>
+        <Select 
+        value={selectedAbteilung} 
+        label="Abteilungen" 
+        style={{minWidth:"150px", maxWidth:"250px"}} 
+        variant='outlined'
+        labelId="abteilungen-select-label" 
+        defaultValue={"Abteilung wählen"}
+        onChange={e=>setSelectedAbteilung(e.target.value)}
+        >
+          {loaded && abteilungen?.map(name=><MenuItem value={name}>{name}</MenuItem>)}
+        </Select>
+      </FormControl>
+      </div>
+
+      <Divider style={{margin:20}}>Buchinfos</Divider>
+
+      <Table sx={{ minWidth: 650 }} aria-label="simple table" >
+        <TableHead>
+          <TableRow>
+            <TableCell>Buch</TableCell>
+            <TableCell>Author</TableCell>
+            <TableCell>ISBN</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {abteilungBooks?.map(
+            (buch)=>{
+              return <TableRow><TableCell>{buch?.buchtitel}</TableCell>
+              <TableCell>{buch.autor[0].vorname[0] + " " + buch.autor[0].nachname[0]}</TableCell>
+              <TableCell>{buch?.isbn}</TableCell>
+              </TableRow>;
+              
+            }
+            )}
+        </TableBody>
+      </Table>
+    </Box>
+    
     </div>
 );
 
